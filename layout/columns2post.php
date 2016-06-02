953e9c919842a2fde944a1960c6b26bb6ccc34d7
wiegerdeleur <wieger@wiegerdeleur.nl>
2016-06-02 13:14:48 +0200

    exit nav button

--- layout/columns2post.php
+++ layout/columns2post.php
@@ -45,12 +45,7 @@ echo $OUTPUT->doctype() ?>
 	<header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex">
 		<nav role="navigation" class="site-nav">
 			<div class="container-fluid">
-				
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
+
 				<div class="nav-collapse collapse">
 					
 					<ul class="nav pull-right">
